ded14ec602bbaf56ddc10cc2a666d2b257e527bc
matt-gamejam <45880823+matt-gamejam@users.noreply.github.com>
2019-04-05 22:17:04 +0200

    lamps

--- arduino/mgst/mgst.ino
+++ arduino/mgst/mgst.ino
@@ -1,5 +1,4 @@
-
-#define LED_PIN 2
+const int _buttonCount = 8;
 
 enum Button {
   Start = 0,
@@ -14,81 +13,128 @@ enum Button {
 
 enum LampMode
 {
-    Off = 0,
-    On = 1,
-    BlinkSlow = 2,
-    BlinkMedium = 3,
-    BlinkFast = 4
+  Off = 0,
+  On = 1,
+  BlinkSlow = 2,
+  BlinkMedium = 3,
+  BlinkFast = 4
 };
 
- byte _firstByte = -1;
+byte _firstByte = -1;
 
- Button [] buttons;
- 
-// Define Function Prototypes that use User Types below here or use a .h file
-//
+boolean buttonState[_buttonCount];
+LampMode lampState[_buttonCount];
 
+int button2ButtonPin(Button button)
+{
+  switch (button)
+  {
+    case Start: return A4;
+    case Autostart: return A3;
+    case SelectGame: return A2;
+    case Info: return A1;
+    case Red: return A0;
+    case Black: return 10;
+    case Payout: return 11;
+    case Service: return 12;
+  }
+}
 
-// Define Functions below here or use other .ino or cpp files
-//
+int button2LedPin(Button button)
+{
+  return button + 2;
+}
 
-// The setup() function runs once each time the micro-controller starts
 void setup()
 {
   Serial.begin(9600);
-  pinMode(LED_PIN, OUTPUT);
 
+  for (int i = 0; i < _buttonCount; i++)
+  {
+    buttonState[i] = false;
+    lampState[i] = Off;
+
+    int ledPin = button2LedPin(i);
+    pinMode(ledPin, OUTPUT);
+
+    int buttonPin = button2ButtonPin(i);
+    pinMode(buttonPin, INPUT_PULLUP);
+  }
 }
 
-int button2led(Button button)
-{
-  return button+2;
-}
-
-// Add the main program code into the continuous loop() function
 void loop()
 {
   checkSerial();
-
   checkButtons();
-
-
-//  digitalWrite(LED_PIN, HIGH);
-//  Serial.write("on\n");
-//  delay(400);
-//
-//  digitalWrite(LED_PIN, LOW);
-//  Serial.write("off\n");
-//  delay(400);
+  applyLamps();
 }
-
 
 void checkSerial()
 {
   while (Serial.available())
   {
-     byte b = Serial.read();
-     
-     processByte(b);
+    byte b = Serial.read();
+    processByte(b);
   }
 }
 
-
 void processByte(byte b)
 {
-    if (_firstByte != -1)
+  if (_firstByte != -1)
+  {
+    if (b == 'l')
     {
-      if (b == 'l')
-      {
-      
-      }
-       Button button = _firstByte;
-       int led = button2led(button);
-       digitalWrite(LED_PIN, HIGH);
+
     }
+    Button button = _firstByte;
+    int led = button2LedPin(button);
+  }
 }
 
 void checkButtons()
 {
+  for (int i = 0; i < _buttonCount; i++)
+  {
+    int sensorValue = digitalRead(button2ButtonPin(i));
+    boolean down = sensorValue == HIGH;
+    if (buttonState[i] != down)
+    {
+      buttonState[i] = down;
+      Serial.write(i);
+      lampState[i] = On;
+    }
+  }
+}
 
+void applyLamps()
+{
+  bool blinkSlow = millis() % 1000 > 0;
+  bool blinkMedium = millis() % 1000 > 0;
+  bool blinkFast = millis() % 1000 > 0;
+
+  for (int i = 0; i < _buttonCount; i++)
+  {
+    int pin = button2LedPin(i);
+    bool on = LOW;
+    switch (lampState[i])
+    {
+      case On:
+        on = HIGH;
+        break;
+      case Off:
+        on = LOW;
+        break;
+      case BlinkSlow:
+        on = blinkSlow;
+        break;
+      case BlinkMedium:
+        on = blinkMedium;
+        break;
+      case BlinkFast:
+        on = blinkFast;
+        break;
+    }
+
+    digitalWrite(pin, on);
+  }
 }
